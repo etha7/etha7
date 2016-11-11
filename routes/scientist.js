2e1d14135662505c5209bd5646632a75bba7bda3
Jennifer Tran <jet054@ucsd.edu>
2016-11-10 21:42:39 -0800

    Updating scientist.js to find correct json file

--- routes/scientist.js
+++ routes/scientist.js
@@ -3,7 +3,7 @@
  * GET role page.
  */
 
-var data1 = require('json/sampleData.json');
+var data1 = require('../public/json/sampleData.json');
 
 exports.view = function(req, res){
 	console.log(data1);
